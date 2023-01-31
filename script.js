6674c252b06fd9ffee2ee4dcdf87da824fad6870
cold | 寒 <83245450+coldroman@users.noreply.github.com>
2023-02-01 00:24:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -60,12 +60,3 @@ window.onload = function () {
     changeOpacity();
 }
 
-
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
-}
-
