84ff67eb5ab9d2272f32ef5d86422276e6d3276e
cold | 寒 <83245450+coldroman@users.noreply.github.com>
2023-02-01 20:13:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -68,4 +68,10 @@ window.onload = function () {
     document.execCommand("copy");
     $temp.remove();
   }
+  // copy coupone code to clipboard
+  $(".coupon-btn").on("click", function() {
+    copyToClipboard("#coupon-field");
+    $(".coupon-alert").fadeIn("slow");
+  })
+})
  
